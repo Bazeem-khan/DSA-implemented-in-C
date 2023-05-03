da09300ec00f67869738621cf67c1072b078e0eb
Bazeem Azad Khan <bazeemkhan16@gmail.com>
2023-05-03 22:47:39 +0530

    Adding new functions

--- DSA[4].cpp
+++ DSA[4].cpp
@@ -1,8 +1,9 @@
+//(LINKED-L.C)
 #include<stdio.h>
 //#include<stddef.h>
-#include<ctype.h>
-#include<stdlib.h>
-//#include<conio.h>
+#include<ctype.h>//for toupper
+#include<stdlib.h>// for malloc
+#include<conio.h>
 
 void add(struct linked **);
 void dis(struct linked *);
@@ -13,6 +14,14 @@ void count_even(struct linked *);
 void odd(struct linked *);
 void even(struct linked *);
 void greatest(struct linked *);
+void smallest(struct linked *);
+void rmv_end(struct linked **);
+void count_nodes(struct linked *);
+void rmv_beg(struct linked **);
+void count_prime(struct linked *);
+void search(struct linked *);
+void search_rep(struct linked *);
+void add_spec_loc(struct linked **);
 
 struct linked
 {
@@ -28,7 +37,7 @@ int main()
 	
 	while(x)
 	{
-		printf("\nEnter your choice   A for add    D for Display    S for sum of even    C for sum of odd    F to count even    I to count odd    O to display odd    H to display even     G to show greatest number    E for exit");
+		printf("\nEnter your choice   A for add    D for Display    S for sum of even    C for sum of odd    F to count even    I to count odd    O to display odd    H to display even     G to show greatest number    R to remove from end    B to count nodes    J to find smallest    K to remove begning    L to count prime    M to search a number    N to search and replace a number    O to add at specific locaion    E for exit");
 		scanf("%c",&c);
 		c=toupper(c);
 		
@@ -67,6 +76,38 @@ int main()
 		else if(c=='G')
 		{
 			greatest(base);
+		}
+		else if(c=='R')
+		{
+			rmv_end(&base);
+		}
+		else if(c=='B')
+		{
+			count_nodes(base);
+		}
+		else if(c=='J')
+		{
+			smallest(base);
+		}
+		else if(c=='K')
+		{
+			rmv_beg(&base);
+		}
+		else if(c=='L')
+		{
+			count_prime(base);
+		}
+		else if(c=='M')
+		{
+			search(base);
+		}
+		else if(c=='N')
+		{
+			search_rep(base);
+		}
+		else if(c=='O')
+		{
+			add_spec_loc(&base);
 		}
 		else if(c=='E')
 		{
@@ -274,6 +315,147 @@ void greatest(struct linked *start)
 			}
 			temp=temp->link;
 		}
-		printf("\n%d",g);
+		printf("\nGreatest number=%d",g);
 	}
+}
+void rmv_end(struct linked **start)
+{
+	struct linked *temp,*pre;
+	temp=*start;
+	
+	if(*start==NULL)
+	{
+		printf("Linked list is empty");
+	}
+	else if(temp->link==NULL)
+	{
+		*start=NULL;
+		printf("Node removed");
+	}
+	else
+	{
+		while(temp->link!=NULL)
+		{
+			pre=temp;
+			temp=temp->link;
+		}
+		pre->link=NULL;
+		printf("Node removed");
+	}
+	
+}
+void count_nodes(struct linked *start)
+{
+	int flag=0;
+	struct linked *temp;
+	
+	if(start==NULL)
+	{
+		printf("\nLinked List is empty");
+	}
+	else
+	{
+		temp=start;
+		while(temp!=NULL)
+		{
+			flag++;	
+			temp=temp->link;
+		}
+		printf("\nNumber of nodes=%d",flag);
+	}
+}
+void smallest(struct linked *start)
+{
+	int s=0;
+	struct linked *temp;
+	temp=start;
+	s=temp->data;
+	
+	if(start==NULL)
+	{
+		printf("\nLinked List is empty");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			if(temp->data<s)
+			{
+				s=temp->data;
+			}
+			temp=temp->link;
+		}
+		printf("Smallest number=%d",s);
+	}
+}
+void rmv_beg(struct linked **start)
+{
+	struct linked *temp;
+	temp=*start;
+	
+	if(*start==NULL)
+	{
+		printf("Linked list is empty");
+	}
+	else
+	{
+		*start=temp->link;
+		printf("Node removed");
+	}
+}
+void count_prime(struct linked *start)
+{
+	int n=2,flag=0;
+	struct linked *temp;
+	temp=start;
+	
+	if(start==NULL)
+	{
+		printf("\nLinked list is empty");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			while(n<=temp->data)
+			{
+				if(temp->data%n==0)
+				{
+					flag++;;
+					break;
+				}
+				n++;
+				
+			}
+			temp=temp->link;
+		}
+		printf("\nNumber of prime numbers=%d",flag);
+	}
+}
+void search(struct linked *start)
+{
+	int x;
+	struct linked *temp;
+	temp=start;
+	
+	if(start==NULL)
+	{
+		printf("\nLinked list is empty");
+	}
+	else
+	{
+		printf("\nEnter the number you want search");
+		scanf("%d",&x);
+		
+		while(temp!=NULL)
+		{
+			
+		}
+	}
+}
+void search_rep(struct linked *start)
+{
+}
+void add_spec_loc(struct linked **start)
+{
 }

61cc6a71d2a8c5368a2f4ca962ae73a6b6122ddd
Bazeem Khan <bazeemkhan16@gmail.com>
2023-05-26 18:35:18 +0530

    Adding new functions in Circular Linked List [1]

--- Circular Linked List [1].cpp
+++ Circular Linked List [1].cpp
@@ -9,8 +9,12 @@ struct linked
 	struct linked *link;
 };
 
+void add_last(struct linked **);//------------------------------L
 void add_beg(struct linked **);//-------------------------------A
 void dis(struct linked *);//------------------------------------D
+void even(struct linked *);//-----------------------------------B
+void odd(struct linked *);//------------------------------------O
+void prime(struct linked *);//----------------------------------P
 
 int main()
 {
@@ -21,7 +25,7 @@ int main()
 	
 	while(x)
 	{
-		printf("\nEnter your choice\nA to add begining    D to Display    E to exit");
+		printf("\nEnter your choice\nA to add at begining    L to Add at last    D to Display    B for Even    O for Odd    P for Prime    E to exit");
 		scanf("%c",&c);
 		c=toupper(c);
 		
@@ -29,9 +33,25 @@ int main()
 		{
 			add_beg(&base);
 		}
+		else if(c=='L')
+		{
+			add_last(&base);
+		}
 		else if(c=='D')
 		{
 			dis(base);
+		}
+		else if(c=='B')
+		{
+			even(base);
+		}
+		else if(c=='O')
+		{
+			odd(base);
+		}
+		else if(c=='P')
+		{
+			prime(base);
 		}
 		else if(c=='E')
 		{
@@ -65,6 +85,31 @@ void add_beg(struct linked **start)
 		*start=temp;
 	}
 }
+//----------------------------------------------TO ADD AT LAST---------------------------------------------//
+void add_last(struct linked **start)
+{
+	struct linked *temp,*pre;
+	
+	temp=(struct linked *)malloc(sizeof(struct linked));
+	printf("\nEnter the number");
+	scanf("%d",&temp->data);
+	
+	if(*start==NULL)
+	{
+		*start=temp;
+		temp->link=temp;
+	}
+	else
+	{
+		temp->link=*start;
+		pre=*start;
+		while(pre->link!=(*start))
+		{
+			pre=pre->link;
+		}
+		pre->link=temp;
+	}
+}
 //-------------------------------------------TO DISPLAY----------------------------------------------//
 void dis(struct linked *start)
 {
@@ -79,8 +124,92 @@ void dis(struct linked *start)
 		do
 		{
 			
-			printf("%d",temp->data);
+			printf("%d  ",temp->data);
 			temp=temp->link;
 		}while(temp!=(start));
 	}
+}
+//--------------------------------------------------TO DISPLAY,SUM AND COUNT EVEN NUMBERS-------------------------------------------------------//
+void even(struct linked *start)
+{
+	int s=0,c=0;
+	struct linked *temp;
+	if(start==NULL)
+	{
+		printf("\nLinked list is empty");
+	}
+	else
+	{
+		temp=start;
+		do
+		{
+			if(temp->data%2==0)
+			{
+				printf("%d  ",temp->data);
+				s+=temp->data;
+				c++;
+			}
+			temp=temp->link;
+		}while(temp!=(start));
+		printf("\nSum of Even numbers=%d\nNumber of Even numbers=%d",s,c);
+	}
+}
+//--------------------------------------------------TO DISPLAY,SUM AND COUNT ODD NUMBERS-------------------------------------------------------//
+void odd(struct linked *start)
+{
+	int s=0,c=0;
+	struct linked *temp;
+	if(start==NULL)
+	{
+		printf("\nLinked list is empty");
+	}
+	else
+	{
+		temp=start;
+		do
+		{
+			if(temp->data%2==1)
+			{
+				printf("%d  ",temp->data);
+				s+=temp->data;
+				c++;
+			}
+			temp=temp->link;
+		}while(temp!=(start));
+		printf("\nSum of Odd numbers=%d\nNumber of Odd numbers=%d",s,c);
+	}
+}
+//------------------------------------------------TO DISPLAY,SUM AND COUNT PRIME NUMBERS-------------------------------------------------------//
+void prime(struct linked *start)
+{
+	int n,s=0,c=0,flag;
+	struct linked *temp;
+	
+	if(start==NULL)
+	{
+		printf("\nLinked list is empty");
+	}
+	else
+	{
+		temp=start;
+		do
+		{
+			n=2;
+			while(n<temp->data)
+			{
+				if(temp->data%n==0)
+				{
+					break;
+				}
+				n++;
+			}
+			if(temp->data==n)
+			{
+				printf("%d  ",temp->data);
+				s+=temp->data;
+				c++;
+			}
+			temp=temp->link;
+		}while(temp!=(start));
+		printf("\nSum of Prime Numbers=%d\nNumber of Prime numbers=%d",s,c);	}
 }
